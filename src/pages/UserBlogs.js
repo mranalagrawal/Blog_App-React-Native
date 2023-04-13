bd5b418e3129b8a6cb4d2efa2b2b35e8663b8ea8
Mranal Agrawal <mranalagrawal.29@gmail.com>
2023-04-13 13:38:43 +0530

    UserBlogs Responsive

--- src/pages/UserBlogs.js
+++ src/pages/UserBlogs.js
@@ -10,7 +10,7 @@ import {
 import React, {useState, useEffect} from 'react';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
-import { horizontalScale } from '../constants/constants';
+import { horizontalScale, verticalScale } from '../constants/constants';
 
 const UserBlogs = ({navigation, route}) => {
 
@@ -82,52 +82,9 @@ const UserBlogs = ({navigation, route}) => {
 export default UserBlogs;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: horizontalScale,
-  },
-  header: {
-    width: horizontalScale,
-    height: 50,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  heading: {
-    marginTop: 5,
-  },
-  headingTxt: {
-    fontSize: 18,
-
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  headingText: {
-    fontSize: 18,
-    color: 'black',
-    fontWeight: '300',
-    alignSelf: 'center',
-  },
-  item: {
-    borderWidth: 0.5,
-    padding: 8,
-    borderRadius: 10,
-    justifyContent: 'center',
-  },
-  categoryTxt: {
-    color: 'black',
-    borderRadius: 15,
-    borderWidth: 0.8,
-    borderColor: 'black',
-    padding: 5,
-    textAlign: 'center',
-    margin: 10,
-    fontSize: 12,
-    fontWeight: '700',
-  },
   image: {
-    width: 300,
-    height: 250,
+    width: horizontalScale(300),
+    height: verticalScale (250),
     alignSelf: 'center',
     marginTop: 15,
     borderRadius: 10,
@@ -144,7 +101,7 @@ const styles = StyleSheet.create({
 
   blog: {
     width: '75%',
-    height: 360,
+   paddingBottom:5,
     backgroundColor: 'white',
     marginTop: 25,
     marginHorizontal: 10,
